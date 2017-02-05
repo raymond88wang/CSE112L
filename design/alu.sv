b457238ae1413dbf14965f04ea4dcf11d7d8e4ac
Raymond Wang <raymond@insellerate.com>
2017-02-04 20:02:14 -0800

    moved files to correct order and fixed alu flags

--- design/alu.sv
+++ design/alu.sv
@@ -1,7 +1,7 @@
 module alu(
     input logic [31:0] A, B,
-    input logic [1:0] opcode,
-    output logic [31:0] result,
+    input logic [1:0] ALUControl,
+    output logic [31:0] ALUResult,
 	output logic [3:0] ALUFlags
     );
 
@@ -15,13 +15,13 @@ module alu(
 		automatic logic zero = 1'b0;
 		ALUFlags = 4'b0000;
 		
-		result = 32'd0;
+		ALUResult = 32'b0;
 		
-		case(opcode)
+		case(ALUControl)
 			// 4'b0000 :  // No OP
 			// 2'b00 :
 			// begin
-				// result = 32'd0;
+				// ALUResult = 32'd0;
 				// overflow = 1'b0;
 				// c_out = 1'b0;
 				// equal = 1'b0;
@@ -30,34 +30,32 @@ module alu(
 			// 4'b0001 :   // ADD
 			2'b00 :
 			begin
-				{c_out,result} = A + B;
-				if (A[31] & B[31] & ~result[31])
+				{c_out,ALUResult} = A + B;
+				if (A[31] & B[31] & ~ALUResult[31])
 					overflow = 1'b1;
-				else if (~A[31] & ~B[31] & result[31])
+				else if (~A[31] & ~B[31] & ALUResult[31])
 					overflow = 1'b1;
 				else
 					overflow = 1'b0;
-				c_out = c_out & opcode[1];
-				zero = ~|result[31];
-				negative = result[31];
+				zero = ~|ALUResult;
+				negative = ALUResult[31];
 			end
 			// 4'b0010 :   // SUB
 			2'b01 :
 			begin
-				{c_out,result} = A - B;
-				if (A[31] & ~B[31] & ~result[31])
+				{c_out,ALUResult} = A - B;
+				if (A[31] & ~B[31] & ~ALUResult[31])
 					overflow = 1'b1;
-				else if (~A[31] & B[31] & result[31])
+				else if (~A[31] & B[31] & ALUResult[31])
 					overflow = 1'b1;
 				else
 					overflow = 1'b0;
-				c_out = c_out & opcode[1];
-				zero = ~|result[31];
-				negative = result[31];
+				zero = ~|ALUResult;
+				negative = ALUResult[31];
 			end
 			// 4'b0011 :   // COMP
 			// begin
-				// result = 32'd0;    
+				// ALUResult = 32'd0;    
 				// c_out = 1'b0;
 				// overflow = 1'b0;
 				// if (A == B)
@@ -68,20 +66,14 @@ module alu(
 			// 4'b0101 :   // AND
 			2'b10 :
 			begin
-				result = A & B;
-				c_out = 1'b0;
-				overflow = 1'b0;
-				negative = 1'b0;
-				zero = 1'b0;
+				ALUResult = A & B;
+				zero = ~|ALUResult;
 			end
 			// 4'b0110 :   // OR
 			2'b11 :
 			begin
-				result = A | B;
-				c_out = 1'b0;
-				overflow = 1'b0;
-				negative = 1'b0;
-				zero = 1'b0;
+				ALUResult = A | B;
+				zero = ~|ALUResult;
 			end
 			// 4'b0111 :   // NOT
 			// begin
@@ -118,6 +110,6 @@ module alu(
 				// equal = 1'b0;
 			// end
 		endcase
-		ALUFlags = {overflow, c_out, negative, zero};
+		ALUFlags = {negative, zero, c_out, overflow};
 	end
 endmodule
